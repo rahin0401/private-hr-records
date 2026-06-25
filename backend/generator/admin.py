3cdd871e7cafb4fd04dfbb470a1b6f881ec6d745
rahin0401 <rahinabuthahir12345@gmail.com>
2026-06-25 13:30:06 +0530

    finised history generation models

--- backend/generator/admin.py
+++ backend/generator/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import SyntheticDataset
 # Register your models here.
+admin.site.register(SyntheticDataset)
--- backend/generator/models.py
+++ backend/generator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from projects.models import Project
 # Create your models here.
+class SyntheticDataset(models.Model):
+    project = models.ForeignKey(Project,on_delete=models.CASCADE, related_name="synthetic_datasets")
+    file = models.FileField(upload_to="synthetic/")
+    rows_generated = models.BigIntegerField()
+    quality_score = models.FloatField(default=0)
+    privacy_score = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.project.name}-{self.created_at}"
